aeb13ab63a3d5cf6f3e76da876e479383188313a
fungodz <128217621+fungodz@users.noreply.github.com>
2026-05-17 03:33:54 +0700

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-# 🌟 ใส่ Firebase Config ของบอสตรงนี้ให้ครบนะครับ
+# 🌟 ใส่ Firebase Config ตรงนี้ให้ครบนะครับ
 firebaseConfig = {
     "apiKey": "AIzaSyDgM6eehnIpIFZ20ZJdvoIvrQEmGklareM",
     "authDomain": "vmax-titan-5d42d.firebaseapp.com",
